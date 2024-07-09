2ce4f99235abb0a9565e75da5869bacd06436958
andresgonzalez <andresfgonzalezr1996@gmail.com>
2024-07-09 16:07:56 -0500

    deleting prints from main

--- main.py
+++ main.py
@@ -1,21 +1,17 @@
 from fastapi import FastAPI
 from utils.models import InputData
-from database.Neural_Salary import predict_salary
+from database.train import predict_salary
 
 app = FastAPI()
 
 
 @app.post("/predict")
 def predict(personal_data: InputData):
-    print(personal_data.dict())
     data = personal_data.dict()
     prediction = predict_salary(data)
-    print(prediction)
-    print(type(prediction)) # from tensor to int
     response = {"message": "salary predicted",
                 "prediction": prediction}
-    print(response)
     return response
 
 
-# run me with uvicorn
+# run me with uvicorn main:app --reload
